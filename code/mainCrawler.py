b5aa8b2882431add91162749528022c42315668f
shiaky <some-body@somewhere.com>
2017-11-02 19:40:44 +0100

    moved files to code

--- code/mainCrawler.py
+++ code/mainCrawler.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
 from SteamCrawler import Player, Game, SteamCrawler
 import datetime
 
-apiKey = "yourApiKeyHere"
+apiKey = ""
 crawler = SteamCrawler(apiKey)
-playersToCrawl = 1000 # takes around 2 hours with crawlingPlayerAchievements enabled
-crawlPlayerAchievements = True  # one api call per game a player owns -> ~ 5-100 api calls per player
+playersToCrawl = 2  # takes around 2 hours with crawlingPlayerAchievements enabled
+# one api call per game a player owns -> ~ 5-100 api calls per player
+crawlPlayerAchievements = True
 players, games = crawler.CrawlSteam(playersToCrawl, crawlPlayerAchievements)
 
+print(players)
+
+print(games)
