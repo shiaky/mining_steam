ce3404be815785186ca98a84df4e9dda3ebb22ed
shiaky <some-body@somewhere.com>
2017-11-05 12:04:19 +0100

    fixed wrong usage of Steam Crawler

--- code/mainCrawler.py
+++ code/mainCrawler.py
@@ -5,7 +5,7 @@ import datetime
 
 apiKey = "38306EC9A593F8A835065D5A8F87BC98"
 crawler = SteamCrawler(apiKey)
-playersToCrawl = 10  # takes around 2 hours with crawlingPlayerAchievements enabled
+playersToCrawl = 4000  # takes around 2 hours with crawlingPlayerAchievements enabled
 # one api call per game a player owns -> ~ 5-100 api calls per player
 crawlPlayerAchievements = True
 
@@ -13,10 +13,8 @@ crawlPlayerAchievements = True
 ##################################################
 db = SteamDbStore("data.db")
 
-for lRound in range(100):
-    print("Round %i, getting %i players total" %
-          (lRound, (lRound + 1) * playersToCrawl))
-    players, games = crawler.CrawlSteam(
-        playersToCrawl, crawlPlayerAchievements)
-    db.insert_games(games)
-    db.insert_players(players)
+print("Getting %i players" % (playersToCrawl))
+players, games = crawler.CrawlSteam(
+    playersToCrawl, crawlPlayerAchievements)
+db.insert_games(games)
+db.insert_players(players)
