ddb5d42cdeaad447ff63db791644fe96c846ae66
shiaky <some-body@somewhere.com>
2018-01-11 21:05:29 +0100

    fixed errors in fetaure generator

--- code/featuregen_genre.py
+++ code/featuregen_genre.py
@@ -7,13 +7,14 @@ import sqlite3
 import os.path
 from multiprocessing.pool import ThreadPool
 import numpy as np
+import time
 
 
 # --- config------
 sOutputFileName = "feature_genre.csv"
 db = "data.db"
-lNumberOfPlayersToTest = 6
-lNumberOfThreads = 3
+lNumberOfPlayersToTest = 10
+lNumberOfThreads = 4
 lNumberOfGenres = 35
 # ----- END -----
 
@@ -24,7 +25,7 @@ bFileExistend = os.path.isfile(sOutputFileName)
 # set ThreadPool
 oPool = ThreadPool(processes=lNumberOfThreads)
 
-
+dStart = time.time()
 # Read sqlite query results into a pandas DataFrame
 con = sqlite3.connect(db)
 # oGames = pd.read_sql_query("SELECT * FROM games", con)
@@ -33,6 +34,7 @@ oFriends = pd.read_sql_query("SELECT * FROM friends", con)
 oOwnedGames = pd.read_sql_query("SELECT * FROM ownedgames", con)
 oGamesGenres = pd.read_sql_query("SELECT * FROM games_genres", con)
 con.close()
+print("read db: %fs" % (time.time() - dStart))
 
 # --------------------------------------------------------------
 
@@ -52,20 +54,22 @@ def get_playtime_of_game_from_player(lPlayerId, lGameId):
 def get_genre_dist_and_totalplaytime_of_player(lPlayerId):
     aGenreDist = np.zeros(lNumberOfGenres)
     aGames = get_owned_games(lPlayerId)
+    # print("get genres of %i games" % (len(aGames)))
     lTotalPlaytime = 0
     for lGameId in aGames:
         aGenreIdx = get_genres_of_game(lGameId)
         lPlaytime = get_playtime_of_game_from_player(lPlayerId, lGameId)
-        lTotalPlaytime += lPlaytime
         # handle the cases that the game is no game but a video and has no
         # genre or that the game was never played
         if len(aGenreIdx) == 0 or lPlaytime == 0:
             continue
+        lTotalPlaytime += lPlaytime
         dSingleGenrePlaytime = lPlaytime / len(aGenreIdx)
         # add playtimes to genres
         for lGenreIdx in aGenreIdx:
             aGenreDist[lGenreIdx - 1] += dSingleGenrePlaytime
-    return aGenreDist / aGenreDist.sum(), lTotalPlaytime
+    aResult = aGenreDist / aGenreDist.sum() if aGenreDist.sum() > 0 else None
+    return aResult, lTotalPlaytime
 
 
 def get_player_friends(lPlayerId):
@@ -90,18 +94,25 @@ def get_owned_games(lPlayerId):
 
 
 def explore_list_of_players(aPlayersToExplore):
-    dicFriendsGameConnectivity = {"playerId": [],
-                                  "playerGenreDistribution": [], "mainGenre": [], "mainGenrePercentage": [], "meanFriendsGenreDistribution": [], "friendsMainGenre": [], "friendsMainGenrePercentage": [], "palyerTotalPlaytime": [], "friendsTotalPlaytime": [], "numberOfGenres": [], "numberOfFriends": []}
+    dicFriendsGameConnectivity = {"playerId": [], "playerGenreDistribution": [], "mainGenre": [], "mainGenrePercentage": [], "meanFriendsGenreDistribution": [], "friendsMainGenre": [], "friendsMainGenrePercentage": [
+    ], "palyerTotalPlaytime": [], "friendsTotalPlaytime": [], "numberOfPlayersGenres": [], "numberOfFriendsTotal": [], "numberOfFriendsConsidered": [], "numberOfFriendsWithoutPlayedGames": []}
 
     for lExploredPlayerId in aPlayersToExplore:
+        dStart = time.time()
         aFriends = get_player_friends_crawled(lExploredPlayerId)
         lNumberOfFriends = len(aFriends)
+        lNumberOfFriendsConsidered = lNumberOfFriends
+        lNumberOfFriendsWithoutPlayedGames = 0
         # handle if no friends
         if lNumberOfFriends == 0:
+            print("player has no crawled friends ... skip player")
             continue
-
         aPlayerGenreDistribution, lPlayerTotalPlaytime = get_genre_dist_and_totalplaytime_of_player(
             lExploredPlayerId)
+        # handle if player has has no played games
+        if aPlayerGenreDistribution is None:
+            print("player has no played games .. skip player")
+            continue
         lMainGenre = aPlayerGenreDistribution.argmax() + 1
         dMainGenrePercentage = aPlayerGenreDistribution.max()
         aMeanFriendsGenreDistribution = np.zeros(lNumberOfGenres)
@@ -109,19 +120,26 @@ def explore_list_of_players(aPlayersToExplore):
         for lFriendId in aFriends:
             aFriendGenreDistribution, lFriendPlaytime = get_genre_dist_and_totalplaytime_of_player(
                 lFriendId)
-            aMeanFriendsGenreDistribution += aFriendGenreDistribution
-            lFriendsTotalPlaytime += lFriendPlaytime
+            # handle if friend has no genre Distribution
+            if aFriendGenreDistribution is not None:
+                aMeanFriendsGenreDistribution += aFriendGenreDistribution
+                lFriendsTotalPlaytime += lFriendPlaytime
+            else:
+                lNumberOfFriendsConsidered -= 1
+                lNumberOfFriendsWithoutPlayedGames += 1
+                print("one friend has no played games")
+        # handle if no friends have played games
+        if aMeanFriendsGenreDistribution.sum() == 0:
+            print("no friend has played games ... skip player")
+            continue
+
         # normalize to mean
-        aMeanFriendsGenreDistribution /= lNumberOfFriends
+        aMeanFriendsGenreDistribution /= lNumberOfFriendsConsidered
         # get index of main genre
         # +1 because genres start with 1
         lFriendsMainGenre = aMeanFriendsGenreDistribution.argmax() + 1
         dFriendsMainGenrePercentage = aMeanFriendsGenreDistribution.max()
         lNumberOfPlayersGenres = np.count_nonzero(aPlayerGenreDistribution)
-        # check whether any percentage is nan what happens at division by zeros
-        if np.isnan(dMainGenrePercentage) or np.isnan(dFriendsMainGenrePercentage):
-            continue
-
         # add data to dataframe
         dicFriendsGameConnectivity["playerId"].append(lExploredPlayerId)
         dicFriendsGameConnectivity["playerGenreDistribution"].append(
@@ -140,20 +158,25 @@ def explore_list_of_players(aPlayersToExplore):
             lPlayerTotalPlaytime)
         dicFriendsGameConnectivity["friendsTotalPlaytime"].append(
             lFriendsTotalPlaytime)
-        dicFriendsGameConnectivity["numberOfGenres"].append(
+        dicFriendsGameConnectivity["numberOfPlayersGenres"].append(
             lNumberOfPlayersGenres)
-        dicFriendsGameConnectivity["numberOfFriends"].append(lNumberOfFriends)
+        dicFriendsGameConnectivity["numberOfFriendsTotal"].append(
+            lNumberOfFriends)
+        dicFriendsGameConnectivity["numberOfFriendsConsidered"].append(
+            lNumberOfFriendsConsidered)
+        dicFriendsGameConnectivity["numberOfFriendsWithoutPlayedGames"].append(
+            lNumberOfFriendsWithoutPlayedGames)
 
-        print("player %i, meanFriendsGenreDistribution: %s, friendsMainGenre: %i, friendsMainGenrePercentage: %f, palyerTotalPlaytime: %f, friendsTotalPlaytime: %f,  noOfGenres: %i, nOfFriends: %i" %
-              (lExploredPlayerId, str(aMeanFriendsGenreDistribution), lFriendsMainGenre, dFriendsMainGenrePercentage, lPlayerTotalPlaytime, lFriendsTotalPlaytime, lNumberOfPlayersGenres, lNumberOfFriends))
+        print("%f, player %i, mg: %i, fg: %i, #f: %i, #fwpg: %i" %
+              ((time.time() - dStart), lExploredPlayerId, lMainGenre,  lFriendsMainGenre, lNumberOfFriends, lNumberOfFriendsWithoutPlayedGames))
     return dicFriendsGameConnectivity
 
 
 # ----- main program ---------
 
 if __name__ == "__main__":
-    dicFriendsGameConnectivity = {"playerId": [],
-                                  "playerGenreDistribution": [], "mainGenre": [], "mainGenrePercentage": [], "meanFriendsGenreDistribution": [], "friendsMainGenre": [], "friendsMainGenrePercentage": [], "palyerTotalPlaytime": [], "friendsTotalPlaytime": [], "numberOfGenres": [], "numberOfFriends": []}
+    dicFriendsGameConnectivity = {"playerId": [], "playerGenreDistribution": [], "mainGenre": [], "mainGenrePercentage": [], "meanFriendsGenreDistribution": [], "friendsMainGenre": [], "friendsMainGenrePercentage": [
+    ], "palyerTotalPlaytime": [], "friendsTotalPlaytime": [], "numberOfPlayersGenres": [], "numberOfFriendsTotal": [], "numberOfFriendsConsidered": [], "numberOfFriendsWithoutPlayedGames": []}
     aPlayersToExploreFulltake = oPlayers.sample(
         n=lNumberOfPlayersToTest, replace=False)["Id"].values
 
@@ -184,10 +207,14 @@ if __name__ == "__main__":
             dicResult["palyerTotalPlaytime"])
         dicFriendsGameConnectivity["friendsTotalPlaytime"].extend(
             dicResult["friendsTotalPlaytime"])
-        dicFriendsGameConnectivity["numberOfGenres"].extend(
-            dicResult["numberOfGenres"])
-        dicFriendsGameConnectivity["numberOfFriends"].extend(
-            dicResult["numberOfFriends"])
+        dicFriendsGameConnectivity["numberOfPlayersGenres"].extend(
+            dicResult["numberOfPlayersGenres"])
+        dicFriendsGameConnectivity["numberOfFriendsTotal"].extend(
+            dicResult["numberOfFriendsTotal"])
+        dicFriendsGameConnectivity["numberOfFriendsConsidered"].extend(
+            dicResult["numberOfFriendsConsidered"])
+        dicFriendsGameConnectivity["numberOfFriendsWithoutPlayedGames"].extend(
+            dicResult["numberOfFriendsWithoutPlayedGames"])
 
     oExportDataFrame = pd.DataFrame(dicFriendsGameConnectivity)
 
@@ -198,9 +225,12 @@ if __name__ == "__main__":
     oExportDataFrame = oExportDataFrame.drop("Id", axis=1)
     # append data if file already existend
     if bFileExistend:
-        oExportDataFrame.to_csv(sOutputFileName, mode='a', header=False)
-    else:
-        oExportDataFrame.to_csv(sOutputFileName)
+        #oExportDataFrame.to_csv(sOutputFileName, mode='a', header=False)
+        oExistingDataFrame = pd.read_csv(sOutputFileName)
+        oExportDataFrame = pd.concat(
+            [oExistingDataFrame, oExportDataFrame], ignore_index=True)
+
+    oExportDataFrame.to_csv(sOutputFileName, index=False)
 
 
 # ----------------------------------------------------------------------
